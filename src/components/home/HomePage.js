0c418ab1894dd9b2b5615ef24868dd3fc6d33a86
Gaurav Kulkarni <gauravkulkarni@Gauravs-MacBook-Pro.local>
2017-02-27 21:08:13 -0700

    small changes

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -8,6 +8,8 @@ class HomePage extends React.Component {
           <h1>React Course in ES6</h1>
           <p> This is a course using React in ES6 and Redux </p>
           <Link to="about" className="btn btn-primary btn-lg"> Learn More </Link>
+          {" "}
+          <Link to="contact" className="btn btn-primary btn-lg"> Contact Us </Link>
         </div>
     );
   }
